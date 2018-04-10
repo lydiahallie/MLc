41279a193e3a06f3bdf769cf24df017baa8f4bdf
Lydia Hallie <lydiajuliettehallie@gmail.com>
2018-04-10 10:02:50 +0200

    Encode categorical data R

--- Ldata_preprocessing.r
+++ Ldata_preprocessing.r
@@ -8,3 +8,7 @@ dataset$Age = ifelse(is.na(dataset$Age),
 dataset$Salary = ifelse(is.na(dataset$Salary),
                         ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                         dataset$Salary)
+
+dataset$Country = factor(dataSet$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
