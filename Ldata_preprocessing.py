432b2d5a1bf88088ace61d592d60f233d90e407b
Lydia Hallie <lydiajuliettehallie@gmail.com>
2018-04-10 09:56:06 +0200

    Encode categorial data

--- Ldata_preprocessing.py
+++ Ldata_preprocessing.py
@@ -18,3 +18,7 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+from sklearn.preprocessing import LabelEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
