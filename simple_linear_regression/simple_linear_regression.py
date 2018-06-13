63f253f88ad6eff9163a68d28a3e27fbfb67ea57
Lydia Hallie <lydiajuliettehallie@gmail.com>
2018-06-13 12:21:05 +0200

    Add multiple linear regression

--- simple_linear_regression/simple_linear_regression.py
+++ simple_linear_regression/simple_linear_regression.py
@@ -10,7 +10,7 @@ from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 from sklearn.linear_model import LinearRegression
-regressor = LienarRegression()
+regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 y_pred = regressor.predict(X_test)
